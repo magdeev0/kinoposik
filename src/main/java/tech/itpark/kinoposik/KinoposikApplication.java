8c5956f62e4cef67f41a2e49e914aba36012dbf7
magdeev0 <bulat@magdeev.net>
2020-12-08 18:17:08 +0300

    heroku

--- src/main/java/tech/itpark/kinoposik/KinoposikApplication.java
+++ src/main/java/tech/itpark/kinoposik/KinoposikApplication.java
@@ -2,6 +2,13 @@ package tech.itpark.kinoposik;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EntityScan("tech.itpark.kinoposik.model")
+@ComponentScan("tech.itpark.kinoposik.controller")
+@EnableJpaRepositories("tech.itpark.kinoposik.repository")
 
 @SpringBootApplication
 public class KinoposikApplication {
