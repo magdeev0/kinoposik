128ac55b92f2a6999b890c0f6e06e550424dfa41
magdeev0 <bulat@magdeev.net>
2020-12-18 22:06:47 +0300

    and one more time

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO actors VALUE (1, 56, 'https://st.kp.yandex.net/im/kadr/3/3/7/kinopoisk.ru-Keanu-Reeves-3379590.jpg', 'Киану Ривз', false);
-INSERT INTO actors VALUE (2, 80, 'https://st.kp.yandex.net/im/kadr/2/5/8/kinopoisk.ru-Al-Pacino-2580555.jpg', 'Аль Пачино', false);
-INSERT INTO actors VALUE (3, 57, 'https://st.kp.yandex.net/im/kadr/1/1/7/kinopoisk.ru-Patrick-Swayze-1178121.jpg', 'Патрик Суэйзи', false);
+INSERT INTO actors VALUES (1, 56, 'https://st.kp.yandex.net/im/kadr/3/3/7/kinopoisk.ru-Keanu-Reeves-3379590.jpg', 'Киану Ривз', false);
+INSERT INTO actors VALUES (2, 80, 'https://st.kp.yandex.net/im/kadr/2/5/8/kinopoisk.ru-Al-Pacino-2580555.jpg', 'Аль Пачино', false);
+INSERT INTO actors VALUES (3, 57, 'https://st.kp.yandex.net/im/kadr/1/1/7/kinopoisk.ru-Patrick-Swayze-1178121.jpg', 'Патрик Суэйзи', false);
