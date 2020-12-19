caacac84c661aab606c9ca0348a902503d7cc0a4
magdeev0 <bulat@magdeev.net>
2020-12-20 01:31:05 +0300

    pom

--- src/main/java/tech/itpark/kinoposik/util/MovieSearchCriteria.java
+++ src/main/java/tech/itpark/kinoposik/util/MovieSearchCriteria.java
@@ -2,21 +2,16 @@ package tech.itpark.kinoposik.util;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.Locale;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class MovieSearchCriteria {
     private String key;
     private String operation;
     private Object value;
-
-    public MovieSearchCriteria(String key, String operation, Object value) {
-        this.key = key.toLowerCase(Locale.ROOT);
-        this.operation = operation;
-        this.value = value;
-    }
 }
