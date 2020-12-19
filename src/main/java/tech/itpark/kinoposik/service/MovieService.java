e3001d3d8464ee8cbaa1ad18bc661b4a0ff4444d
magdeev0 <bulat@magdeev.net>
2020-12-20 01:49:35 +0300

    war with heroku #3

--- src/main/java/tech/itpark/kinoposik/service/MovieService.java
+++ src/main/java/tech/itpark/kinoposik/service/MovieService.java
@@ -160,7 +160,7 @@ public class MovieService {
                 parsedYearEnd = Integer.parseInt(yearEnd);
         }
         MovieSpecification spec1 =
-                new MovieSpecification(new MovieSearchCriteria("name", ":", name+"%"));
+                new MovieSpecification(new MovieSearchCriteria("name", ":", "%"+name+"%"));
         MovieSpecification spec2 =
                 new MovieSpecification(new MovieSearchCriteria("year", ">", parsedYearStart));
         MovieSpecification spec3 =
